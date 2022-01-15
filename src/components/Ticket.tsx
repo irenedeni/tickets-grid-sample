240c8e9cace8258241cce2219d8d01e023f5ecb7
Irene De Nicolo <irene.denicolo89@gmail.com>
2022-01-15 13:04:48 +0100

    added allPerks var, that will be used for rendering the grid rows

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -2,17 +2,52 @@ import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
 export interface ITicket {
-  ticketName?: string
+  ticketActualPrice: string
+  ticketButtonLabel: string
+  ticketButtonLink: string
+  ticketDescription: string
+  ticketDiscount?: string
+  ticketHighlighted: boolean
+  ticketName: string
+  ticketPerks: Array<string | any>
+  ticketStrikethroughPrice?: string;
+  ticketTopLabel?: string
 }
 
-const Ticket = (props: ITicket) => {
+interface ITicketPerk {
+  ticketPerks: string
+}
 
+
+const Ticket = (props: ITicket) => {
+console.log("TicketPerks per ticket", props.ticketPerks)
   return (
-    <TestDiv>{props.ticketName}</TestDiv>
+    <TestDiv>
+      {props.ticketActualPrice}
+      {props.ticketButtonLabel}
+      {props.ticketButtonLink}
+      {props.ticketDescription}
+      {props.ticketHighlighted}
+      {props.ticketName}
+      {props.ticketPerks.map((perk, ind) => 
+        <div style={{color: "black"}} key={ind}>{perk.ticketPerk}</div>
+      )}
+      {props.ticketDiscount &&
+        props.ticketDiscount
+      }
+      {props.ticketStrikethroughPrice &&
+        props.ticketStrikethroughPrice
+      }
+      {props.ticketTopLabel &&
+        props.ticketTopLabel
+      }
+    </TestDiv>
   )
 }
 
 const TestDiv = styled.div`
+  display: flex;
+  flex-direction: column;
   font-size: 1.5em;
   text-align: center;
   color: ${({theme})=> theme.primary};
--- src/slices/TicketsSlice.tsx
+++ src/slices/TicketsSlice.tsx
@@ -20,6 +20,11 @@ const TicketsSlice = () => {
         return res.json()
       })
       .then(function(json) {
+        // Given the following:
+        // 1. In this case, the data and the structure passed by JSON is not going to change;
+        // 2. We are only interested in a specific ticket group, the “In-person + Digital”;
+        // I am assigning to the ticketGroup variable the second item of the data array 
+        // (specifically, the array "tickets" property only).
         const ticketGroup:ITicket[] = json.data[1].tickets
         setData(ticketGroup)
       })
@@ -30,15 +35,33 @@ const TicketsSlice = () => {
   },[])
 
   console.log("data!!!",data)
+  const allPerks:string[] = []
 
   return (
     <>
-      {data.length && data.map((ticket:ITicket, index) => 
-        <Ticket 
-          key={index}
-          ticketName={ticket?.ticketName}
-        />
-      )}
+      {data.length && data.map((ticket:ITicket, index) => {
+        const perks = ticket.ticketPerks
+        for(let i = 0; i < perks.length; i++){
+          if(!allPerks.includes(perks[i].ticketPerk)){
+            allPerks.push(perks[i].ticketPerk)
+          }
+        }
+        return (
+          <Ticket 
+            key={index}
+            ticketName={ticket?.ticketName}
+            ticketActualPrice={ticket?.ticketActualPrice}
+            ticketButtonLabel={ticket?.ticketButtonLabel}
+            ticketButtonLink={ticket?.ticketButtonLink}
+            ticketDescription={ticket?.ticketDescription}
+            ticketHighlighted={ticket?.ticketHighlighted}
+            ticketPerks={ticket?.ticketPerks}
+            ticketDiscount={ticket?.ticketDiscount}
+            ticketStrikethroughPrice={ticket?.ticketStrikethroughPrice}
+            ticketTopLabel={ticket?.ticketTopLabel}
+          />
+        )
+        })}
     </>
     
   )
